de00c324d545d0935f8273721df031ecbeeeddb8
Kyaw Khant Zin <kyawkhantzin.jdc2022@gmail.com>
2024-05-25 16:12:35 +0630

    fff

---
+++ helloworld/src/helloworld/Main.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("I'm Theint Theint Thu");
+		
+		for (int i = 0; i < 1500; i++) {
+			System.out.println("I Love You!!!");
+		}
+		
+		Main main = new Main();
+		main.greet();
+	}
+	
+	void greet() {
+		System.out.println("Hello, こんにちわ , မဂ်လာပါ");
+	}
+
+}
